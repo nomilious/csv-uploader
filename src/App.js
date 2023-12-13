7645d5d89dc101836f105567d09842c17428ed5e
nomilious <eduard.belic.2002@gmail.com>
2023-12-13 21:06:01 +0300

    Minor changes

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import MainScreen from "./components/MainScreen";
 import DataViewerScreen from "./components/DataViewer";
 import {useEffect, useState} from "react";
 import './App.css';
+import transformText from "./utils/csvUtils";
 
 function App() {
     const [data, setData] = useState(null);
@@ -14,51 +15,6 @@ function App() {
             setData(JSON.parse(savedData));
         }
     }, []);
-
-    const transformText = (text) => {
-        // split by new line
-        const rows = text.replaceAll('\r\n', "\n").split("\n");// because of different end on macOs and Windows
-
-
-        // to parse right: split by address, then split the part before it by commas
-        return rows.map(row => {
-            const parts = row.split('"'); // Split by address
-
-            // If there are quotes present
-            if (parts.length > 1) {
-                // part needed to be splitted by commax
-                const firstPart = parts[0].split(',').slice(0, -1);
-                // The address part with potential commas
-                const address = parts[1];
-
-                // Combine the parsed first part with the address
-                return [...firstPart, address];
-            } else {
-                // If no quotes, split the row by commas directly
-                return row.split(',');
-            }
-        });
-
-
-        // Extract the field names from the first row
-        // const fields = rows[0].split(',');
-        //
-        // const jsonData = [];
-        //
-        // for (let i = 1; i < rows.length; i++) {
-        //     const values = rows[i].split(',');
-        //
-        //     // Create an object to store values with field names as keys
-        //     const obj = {};
-        //     for (let j = 0; j < fields.length; j++) {
-        //         obj[fields[j]] = values[j];
-        //     }
-        //
-        //     // Push the object into the JSON data array
-        //     jsonData.push(obj);
-        // }
-        // return jsonData;
-    }
 
     // used in MainScreen
     const handleFileLoad = (file) =>{
@@ -74,7 +30,7 @@ function App() {
             const transformedText = transformText(text);
 
             setData(transformedText);
-            // "upload" to localStorage
+            // upload to localStorage
             localStorage.setItem('data', JSON.stringify(transformedText));
         }
         reader.readAsArrayBuffer(file);
