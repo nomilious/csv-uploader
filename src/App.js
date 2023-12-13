0495ee005846cb26e70db604fb6d0c5b536c2558
nomilious <eduard.belic.2002@gmail.com>
2023-12-13 23:55:27 +0300

    Revert "Added more test. Code coverage = 70%"
    This reverts commit e3919e96

--- src/App.js
+++ src/App.js
@@ -3,12 +3,9 @@ import DataViewerScreen from "./components/DataViewer";
 import {useEffect, useState} from "react";
 import './App.css';
 import transformText from "./utils/csvUtils";
-import {BrowserRouter as Router, Route, Routes, useNavigate} from 'react-router-dom';
-
 
 function App() {
     const [data, setData] = useState(null);
-    const navigate  = useNavigate();
     //TODO add "loading state" while transforming text and reading csv
 
 
@@ -20,19 +17,14 @@ function App() {
     }, []);
 
     // used in MainScreen
-    const handleFileLoad =  (file) =>{
+    const handleFileLoad = (file) =>{
         const reader = new FileReader();
         reader.onload = function (e) {
             // Get the ArrayBuffer of the file content
             const arrayBuffer = e.target.result;
+
             // Decode using encoding windows-1251
             const decoder = new TextDecoder('windows-1251');
-            // try {
-            //     decoder = new TextDecoder('windows-1251');
-            //     // Your decoding logic here
-            // } catch (error) {
-            //     console.error('Error occurred during decoding:', error);
-            // }
             const text = decoder.decode(arrayBuffer);
 
             const transformedText = transformText(text);
@@ -40,9 +32,6 @@ function App() {
             setData(transformedText);
             // upload to localStorage
             localStorage.setItem('data', JSON.stringify(transformedText));
-
-            // navigate
-            navigate('/data-viewer');
         }
         reader.readAsArrayBuffer(file);
     };
@@ -51,24 +40,22 @@ function App() {
     const handleReset = () => {
         setData(null);
         localStorage.removeItem('data');
-        navigate('/');
-
     };
 
     // if file not chosen => MainScreen, else DataViewerScreen
-    return (
-        <div className="app">
-            <Routes>
-                {/* Render different components based on the condition */}
-                {data ? (
-                    <Route path="/data-viewer" element={<DataViewerScreen data={data} onReset={handleReset} />} />
-                ) : (
-                    <Route path="/" element={<MainScreen onFileLoad={handleFileLoad} />} />
-                )}
-            </Routes>
-        </div>
-    );
-
+    if (data) {
+        return (
+            <div className="app">
+                <DataViewerScreen data={data} onReset={handleReset} />
+            </div>
+        );
+    } else {
+        return (
+            <div className="app">
+                <MainScreen onFileLoad={handleFileLoad} />
+            </div>
+        );
+    }
 }
 
 export default App;
